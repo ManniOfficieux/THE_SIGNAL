7efd9a779937cc2c0910b2af99fc63a497780e5a
ManniOfficieux <m.master.dev@gmail.com>
2025-06-17 13:22:07 +0200

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,4 +7,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  <div style="margin-top: 1rem; font-size: 0.7rem; opacity: 0.5;">
+  Ils observent. Tu es peut-être le prochain.
+</div>
+
